1d770b5d91fb477a56ba43c797289deedbe56bf4
Emil Hansen <emilgth@gmail.com>
2019-03-05 13:53:54 +0100

    usermapper v1

---
+++ src/main/java/controllers/GetUsers.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(name = "GetUsers", urlPatterns = "/GetUsers")
+public class GetUsers extends javax.servlet.http.HttpServlet {
+
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
--- src/main/java/mappers/UserMapper.java
+++ src/main/java/mappers/UserMapper.java
@@ -40,7 +40,11 @@ public class UserMapper {
 
             }
             for (Order order : user.getOrders()) {
-                preparedStatement = connection.prepareStatement("SELECT order_id, amount, base_name, topping_name, base_price + topping_price as 'price' from order_lines inner join base b on order_lines.base_id = b.base_id inner join topping t on order_lines.topping_id = t.topping_id where order_id = ?");
+                preparedStatement = connection.prepareStatement("SELECT order_id, amount, base_name, topping_name, " +
+                        "base_price + topping_price as 'price' from order_lines " +
+                        "inner join base b on order_lines.base_id = b.base_id " +
+                        "inner join topping t on order_lines.topping_id = t.topping_id " +
+                        "where order_id = ?");
                 preparedStatement.setInt(1, order.getOrderID());
                 resultSet = preparedStatement.executeQuery();
                 while (resultSet.next()) {
