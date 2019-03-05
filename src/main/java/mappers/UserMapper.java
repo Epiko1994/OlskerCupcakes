1a3a97a2d605a3500f50d10da03ecb752157ac34
Emil Hansen <emilgth@gmail.com>
2019-03-05 13:47:19 +0100

    usermapper v1

--- src/main/java/mappers/UserMapper.java
+++ src/main/java/mappers/UserMapper.java
@@ -1,5 +1,7 @@
 package mappers;
 
+import model.Cupcake;
+import model.Order;
 import model.User;
 import util.ConnnectionConfiguration;
 
@@ -33,7 +35,18 @@ public class UserMapper {
             preparedStatement.setInt(1, user.getUserID());
             resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
+                Order order = new Order(resultSet.getInt("order_id"), resultSet.getString("date"));
+                user.getOrders().add(order);
 
+            }
+            for (Order order : user.getOrders()) {
+                preparedStatement = connection.prepareStatement("SELECT order_id, amount, base_name, topping_name, base_price + topping_price as 'price' from order_lines inner join base b on order_lines.base_id = b.base_id inner join topping t on order_lines.topping_id = t.topping_id where order_id = ?");
+                preparedStatement.setInt(1, order.getOrderID());
+                resultSet = preparedStatement.executeQuery();
+                while (resultSet.next()) {
+                    Cupcake cupcake = new Cupcake(resultSet.getString("topping_name"), resultSet.getString("base_name"), resultSet.getInt("price"), resultSet.getInt("amount"));
+                    order.getCupcakes().add(cupcake);
+                }
             }
         }
 
